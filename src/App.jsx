c02f9007dedcff4ab884b062f3f70904f3b3717c
karera-o <olivierkarera2020@gmail.com>
2022-07-29 09:07:11 +0200

    finishing signUp page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css';
 import { NavSide } from './ui/components/navSide/navSide';
+import { SignUp } from './ui/components/signUp/signup';
+import { SignInPage } from './ui/pages/signInPage/signInPage';
 
 function App() {
   return (
     <div className="App">
          
-      <div id='appLeftSide'>
+      {/* <div id='appLeftSide'>
         <NavSide/>
       </div>
-      <div>
-
-      </div>
-
+      <div id='appRightSide'>
+        
+      </div> */}
+      <SignInPage/>
     </div>
   );
 }
