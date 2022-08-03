091bcd511167a3e88285e086f06126f36bf506a9
karera <olivierkarera2020@gmail.com>
2022-08-03 10:11:53 +0200

    create the rightSide of the home page

--- src/ui/components/rightSide/rightSide.jsx
+++ src/ui/components/rightSide/rightSide.jsx
@@ -4,7 +4,9 @@ function RightSide(){
     return(
         <div
             id='suggestionContent'>
-             div   
+             <div>
+                
+                </div>   
         </div>
         
 
